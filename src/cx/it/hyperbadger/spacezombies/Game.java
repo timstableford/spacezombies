ba9e3624527bc823c730c99573473d4e9a0327b0
tim <tim@d2b39ab5-011e-4231-92f0-84e917f8dfaf>
2012-08-19 15:39:16 +0000

    texture experiment

--- src/cx/it/hyperbadger/spacezombies/Game.java
+++ src/cx/it/hyperbadger/spacezombies/Game.java
@@ -1,14 +1,21 @@
 package cx.it.hyperbadger.spacezombies;
 
+import java.io.IOException;
+
 import org.lwjgl.LWJGLException;
 import org.lwjgl.Sys;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.DisplayMode;
 import org.lwjgl.opengl.GL11;
+import org.newdawn.slick.opengl.Texture;
+import org.newdawn.slick.opengl.TextureLoader;
+import org.newdawn.slick.util.ResourceLoader;
+
 import static org.lwjgl.opengl.GL11.*;
 @SuppressWarnings("unused")
 public class Game {
 	private int fps, lastFPS;
+	private Texture wood = null;
 	public Game(){
 		try {
 			Display.setDisplayMode(new DisplayMode(800,600));
@@ -23,6 +30,9 @@ public class Game {
 		glLoadIdentity();
 		glOrtho(0, 800, 600, 0, 1, -1);
 		glMatrixMode(GL_MODELVIEW);
+		glEnable(GL_TEXTURE_2D);
+		//load textures
+		wood = getTexture("wood.png");
 		//initialize loop
 		while(!Display.isCloseRequested()){
 			loop();
@@ -34,17 +44,30 @@ public class Game {
 		//clear
 		GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);	
 		//draw
-		
 		//draw quad 
 		glBegin(GL_QUADS);
+			wood.bind();
+			glTexCoord2f(0,0);
 	    	glVertex2f(100,100); //topleft
+	    	glTexCoord2f(1,0);
 	    	glVertex2f(100+200,100); //top right
+	    	glTexCoord2f(1,1);
 	    	glVertex2f(100+200,100+200); //bottom right
+	    	glTexCoord2f(0,1);
 	    	glVertex2f(100,100+200); //bottom left
 	    glEnd();
 	    
 		//update
 		Display.update();
 		Display.sync(60);
+	}
+	public Texture getTexture(String name){
+			Texture t = null;
+			try {
+				t = TextureLoader.getTexture("PNG", ResourceLoader.getResourceAsStream("res/"+name));
+			} catch (IOException e) {
+				System.err.println("Could not load texture: "+name);
+			}
+			return t;
 	}
 }
