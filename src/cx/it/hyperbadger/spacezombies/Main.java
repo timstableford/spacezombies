60f19b64e869174d4ea8dec3c7c1836462e4732a
tim <tim@d2b39ab5-011e-4231-92f0-84e917f8dfaf>
2012-08-19 14:56:37 +0000

    staretd spacezombies with lwjgl included

---
+++ src/cx/it/hyperbadger/spacezombies/Main.java
@@ -0,0 +1,13 @@
+package cx.it.hyperbadger.spacezombies;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
