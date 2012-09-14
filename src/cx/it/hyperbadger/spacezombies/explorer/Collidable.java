757aaa3f6955119699a716c77dd2428d2ebb21c9
tim <tim@d2b39ab5-011e-4231-92f0-84e917f8dfaf>
2012-09-14 16:40:11 +0000

    Updated collidable interface

--- src/cx/it/hyperbadger/spacezombies/explorer/Collidable.java
+++ src/cx/it/hyperbadger/spacezombies/explorer/Collidable.java
@@ -1,5 +1,9 @@
 package cx.it.hyperbadger.spacezombies.explorer;
 
+import cx.it.hyperbadger.spacezombies.Vector2d;
+
 public interface Collidable {
-	public boolean collide(Collidable object);
+	public Vector2d getLocation();
+	public double getCollisionRadius();
+	public void collision(Collidable object);
 }
